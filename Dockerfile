1b8412d99df9efba5ef202ab1dce20341db4f386
Dave Hoffman <protozbass@gmail.com>
2019-02-24 12:57:07 -0600

    With the refactor, I removed the routes

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM tiangolo/uwsgi-nginx-flask:python3.7
 
 MAINTAINER Dave Hoffman
 
-COPY ./app /app
-RUN ls -alh /app
+COPY ./app/requirements.txt /app/requirements.txt
 RUN pip3 install -r /app/requirements.txt
+
+COPY ./app/ /app/
--- app/cruddybeer/views.py
+++ app/cruddybeer/views.py
@@ -1,15 +1,22 @@
-from flask import Blueprint, request, redirect, render_template, url_for
+from flask import Blueprint, render_template
 from flask.views import MethodView
-from cruddybeer.models import Beer, Brewery
+from cruddybeer.models import Brewery
 
 breweries = Blueprint("breweries", __name__, template_folder="templates")
+
+
+@breweries.route('/')
+def index():
+    return render_template("index.html", methods=['GET'])
 
 
 class ListBreweriesView(MethodView):
     def __init__(self):
         pass
 
-    def get_brewery_list(self):
+    @staticmethod
+    @breweries.route('/breweries', methods=['GET'])
+    def get_brewery_list():
         all_breweries = Brewery.objects.all()
         return render_template("list.html", breweries=all_breweries)
 
@@ -18,7 +25,8 @@ class DetailBreweriesView(MethodView):
     def __init__(self):
         pass
 
-    def get_brewery_detail(self, slug):
+    @staticmethod
+    def get_brewery_detail(slug):
         brewery = Brewery.objects.get_or_404(slug=slug)
         return render_template("detail.html", brewery=brewery)
 
